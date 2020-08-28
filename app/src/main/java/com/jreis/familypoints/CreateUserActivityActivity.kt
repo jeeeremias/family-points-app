24619b4183efc777308074802d5a888848954f87
Jeremias Reis <jeremias.reis@symphony.com>
2020-08-28 17:22:10 -0300

    Impoved the user routine screen, implementing new user activity in one day

---
+++ app/src/main/java/com/jreis/familypoints/CreateUserActivityActivity.kt
@@ -0,0 +1,11 @@
+package com.jreis.familypoints
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateUserActivityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user_activity)
+    }
+}
--- app/src/main/java/com/jreis/familypoints/UserRoutineActivity.kt
+++ app/src/main/java/com/jreis/familypoints/UserRoutineActivity.kt
@@ -1,7 +1,11 @@
 package com.jreis.familypoints
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -21,6 +25,10 @@ class UserRoutineActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_routine)
 
+        setSupportActionBar(findViewById(R.id.user_routine_toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
         val user = intent.getSerializableExtra("user") as User
 
         val userRoutineDatabase = database.getReference("user_routines/" + user.id + "/monday")
@@ -32,19 +40,12 @@ class UserRoutineActivity : AppCompatActivity() {
 
             override fun onDataChange(routineItemsSnapshot: DataSnapshot) {
                 for (routineItem in routineItemsSnapshot.children) {
-                    database.getReference("activities/" + routineItem.getValue(Long::class.java)).addListenerForSingleValueEvent(object: ValueEventListener {
-                        override fun onCancelled(p0: DatabaseError) {
-                            TODO("Not yet implemented")
-                        }
 
-                        override fun onDataChange(activitySnapshot: DataSnapshot) {
-                            activitySnapshot.getValue(Task::class.java)?.let {
-                                it.time = routineItem.key.toString()
-                                it.assignee = user.firstName
-                                tasks.add(it)
-                            }
-                        }
-                    })
+                    routineItem.getValue(Task::class.java)?.let {
+                        it.time = routineItem.key.toString()
+                        it.assignee = user.firstName
+                        tasks.add(it)
+                    }
                 }
                 Thread.sleep(300)
                 recyclerView_routine.apply {
@@ -57,5 +58,30 @@ class UserRoutineActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.activity_user_routine, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_add_user_activity -> {
+            openCreateActivity()
+        }
+
+        else -> {
+            onHomeUpOptionSelected()
+        }
+    }
+
+    private fun openCreateActivity(): Boolean {
+        val intent = Intent(baseContext, CreateUserActivityActivity::class.java)
+        startActivity(intent)
+        return true
+    }
+
+    private fun onHomeUpOptionSelected(): Boolean {
+        super.onBackPressed()
+        return true;
+    }
 }
